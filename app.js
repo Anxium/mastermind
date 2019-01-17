1e11600e54be0100a007442b44df0e6890e61a1c
Axel Avaux <axel.avx@gmail.com>
2019-01-17 16:00:29 +0100

    bug????

--- app.js
+++ app.js
@@ -1,37 +1,82 @@
 let reponse = document.querySelectorAll('.actual div');
 let couleur = document.querySelectorAll('.choice div');
 let historique = document.querySelectorAll('.historique div');
+let reset = document.querySelector('.reset');
+
+const solution = ['vert', 'noir', 'noir', 'bleu'];
+let solution1 = solution.slice();
 
 let count = 0;
-
-const solution = ['jaune', 'vert', 'rouge', 'jaune'];
 let search = new Array();
 let compare = new Array();
+let boule = new Array();
+let bouleR = 0;
+let bouleN = 0;
+// let bouleC = 0;
 
-for(let i = 0; i<couleur.length; i++) {
+for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
     couleur[i].addEventListener('click', () => {
-        reponse[count].className = couleur[i].className;
-        search.push(couleur[i].className);
-        
-        if(search[count] == solution[count]) {
+
+        reponse[count].className = couleur[i].className;  // Ajoute la couleur sur laquelle on clique dans la "réponse" (css)
+        search.push(couleur[i].className); // Ajoute la couleur sur laquelle on a cliqué dans le tableau
+
+        if(search[count] == solution1[count]) {
             compare.push(search[count]);
+            bouleN++;
+            solution1[count] = "";
         }
-        
+
         count++;
         
-        if(count>=4) { // Condition : Rempli l'historique + réinitialise les réponses + reset les tableaux et le count
+        if(count>=4) {
+        
+            for(let z=0; z<solution1.length; z++) {
+                solution1.filter(element => {
+                    if(element == search[z]) {
+                        bouleR++;
+                        search[z] = "";
+                    }
+                })
+            }
+
+            // console.log( `Boule rouge : ${bouleR} - Boule noir : ${bouleN}` )
+            // bouleC = bouleR - bouleN;
+
+            for(let w=0; w<bouleN; w++) {
+                boule.push('noir');
+            }
+
+            for(let l=0; l<bouleR; l++) {
+                boule.push('rouge');
+            }
+
             for(let y=0; y<reponse.length; y++) {
                 historique[y].className = reponse[y].className;
                 reponse[y].className = "";
+                historique[y+4].className = boule[y];
             }
+
             count = 0;
             search = [];
             compare = [];
+            boule = [];
+            bouleR = 0;
+            bouleN = 0;
+            solution1 = solution.slice();
         }
-
     })
 }
 
-
-// Push comparaison dans un tableau et comparer ce tableau a la solution 
-// Ajouter un tableau qui vérifie si une réponse est présente dans le tableau
+// Bouton reset
+reset.addEventListener('click', () => {
+    reponse.forEach(r => {
+        r.className = "";
+        count = 0;
+        search = [];
+        compare = [];
+        boule = [];
+        bouleR = 0;
+        bouleN = 0;
+        solution1 = solution.slice();
+    })
+});
