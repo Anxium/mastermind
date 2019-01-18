8a06561e04f85ae9f4047c2a0d3cecd6cd24198b
Axel Avaux <axel.avx@gmail.com>
2019-01-18 09:48:41 +0100

    Mastermiiiind

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ let couleur = document.querySelectorAll('.choice div');
 let historique = document.querySelectorAll('.historique div');
 let reset = document.querySelector('.reset');
 
-const solution = ['vert', 'noir', 'noir', 'bleu'];
+const solution = ['jaune', 'noir', 'bleu', 'rouge'];
 let solution1 = solution.slice();
 
-let count = 0;
 let search = new Array();
-let compare = new Array();
 let boule = new Array();
+let count = 0;
 let bouleR = 0;
 let bouleN = 0;
-// let bouleC = 0;
+let bouleC = 0;
 
 for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
     couleur[i].addEventListener('click', () => {
@@ -21,32 +20,32 @@ for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
         search.push(couleur[i].className); // Ajoute la couleur sur laquelle on a cliqué dans le tableau
 
         if(search[count] == solution1[count]) {
-            compare.push(search[count]);
-            bouleN++;
-            solution1[count] = "";
+            bouleN+=1;
+            solution1[count] = "empty";
+            search[count] = "empty";
         }
 
         count++;
         
         if(count>=4) {
-        
+
             for(let z=0; z<solution1.length; z++) {
-                solution1.filter(element => {
-                    if(element == search[z]) {
+                for(let m=0; m<search.length; m++) {
+                    if(search[z] == solution1[m]) {
                         bouleR++;
                         search[z] = "";
+                        solution1[m] = bouleR;
                     }
-                })
+                }
             }
 
-            // console.log( `Boule rouge : ${bouleR} - Boule noir : ${bouleN}` )
-            // bouleC = bouleR - bouleN;
+            bouleC = bouleR - bouleN;
 
             for(let w=0; w<bouleN; w++) {
                 boule.push('noir');
             }
 
-            for(let l=0; l<bouleR; l++) {
+            for(let l=0; l<bouleC; l++) {
                 boule.push('rouge');
             }
 
@@ -58,10 +57,10 @@ for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
 
             count = 0;
             search = [];
-            compare = [];
             boule = [];
             bouleR = 0;
             bouleN = 0;
+            bouleC = 0;
             solution1 = solution.slice();
         }
     })
@@ -71,12 +70,11 @@ for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
 reset.addEventListener('click', () => {
     reponse.forEach(r => {
         r.className = "";
-        count = 0;
-        search = [];
-        compare = [];
-        boule = [];
-        bouleR = 0;
-        bouleN = 0;
-        solution1 = solution.slice();
     })
+    count = 0;
+    search = [];
+    boule = [];
+    bouleR = 0;
+    bouleN = 0;
+    solution1 = solution.slice();
 });
