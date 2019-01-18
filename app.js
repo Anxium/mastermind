c66891f5365eac1cf777beba3cd7e52df5dd4f17
Axel Avaux <axel.avx@gmail.com>
2019-01-18 15:24:50 +0100

    improvement

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Initialisation des données
 let reponse = document.querySelectorAll('.actual div');
 let couleur = document.querySelectorAll('.choice div');
 let historique = document.querySelectorAll('.historique div');
 let reset = document.querySelector('.reset');
 
-const solution = ['jaune', 'noir', 'bleu', 'rouge'];
-let solution1 = solution.slice();
+let solution = new Array();
+for(let r=0; r<4; r++) {
+    random = Math.floor(Math.random() * Math.floor(couleur.length));
+    solution.push(couleur[random].className);
+}
 
 let search = new Array();
 let boule = new Array();
@@ -12,69 +16,74 @@ let count = 0;
 let bouleR = 0;
 let bouleN = 0;
 let bouleC = 0;
+let solution1 = solution.slice();
 
-for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
-    couleur[i].addEventListener('click', () => {
-
-        reponse[count].className = couleur[i].className;  // Ajoute la couleur sur laquelle on clique dans la "réponse" (css)
-        search.push(couleur[i].className); // Ajoute la couleur sur laquelle on a cliqué dans le tableau
-
-        if(search[count] == solution1[count]) {
-            bouleN+=1;
-            solution1[count] = "empty";
-            search[count] = "empty";
-        }
-
-        count++;
-        
-        if(count>=4) {
-
-            for(let z=0; z<solution1.length; z++) {
-                for(let m=0; m<search.length; m++) {
-                    if(search[z] == solution1[m]) {
-                        bouleR++;
-                        search[z] = "";
-                        solution1[m] = bouleR;
-                    }
-                }
-            }
-
-            bouleC = bouleR - bouleN;
-
-            for(let w=0; w<bouleN; w++) {
-                boule.push('noir');
-            }
-
-            for(let l=0; l<bouleC; l++) {
-                boule.push('rouge');
-            }
-
-            for(let y=0; y<reponse.length; y++) {
-                historique[y].className = reponse[y].className;
-                reponse[y].className = "";
-                historique[y+4].className = boule[y];
-            }
-
-            count = 0;
-            search = [];
-            boule = [];
-            bouleR = 0;
-            bouleN = 0;
-            bouleC = 0;
-            solution1 = solution.slice();
-        }
-    })
-}
-
-// Bouton reset
-reset.addEventListener('click', () => {
-    reponse.forEach(r => {
-        r.className = "";
-    })
+// Fonction de reset de données
+const dataReset = () => {
     count = 0;
     search = [];
     boule = [];
     bouleR = 0;
     bouleN = 0;
+    bouleC = 0;
     solution1 = solution.slice();
-});
+}
+
+const jeu = () => {
+    for(let i = 0; i<couleur.length; i++) { // Boucle sur toutes les couleurs
+        couleur[i].addEventListener('click', () => {
+    
+            reponse[count].className = couleur[i].className;
+            search.push(couleur[i].className);
+    
+            if(search[count] == solution1[count]) {
+                bouleN++;
+                solution1[count] = "";
+                search[count] = "";
+            }
+    
+            count++;
+            
+            if(count>=4) {
+    
+                for(let z=0; z<solution1.length; z++) {
+                    for(let m=0; m<search.length; m++) {
+                        if(search[z] == solution1[m]) {
+                            bouleR++;
+                            search[z] = "";
+                            solution1[m] = bouleR;
+                        }
+                    }
+                }
+    
+                bouleC = bouleR - bouleN;
+    
+                for(let w=0; w<bouleN; w++) {
+                    boule.push('noir');
+                }
+    
+                for(let l=0; l<bouleC; l++) {
+                    boule.push('rouge');
+                }
+    
+                for(let y=0; y<reponse.length; y++) {
+                    historique[y].className = reponse[y].className;
+                    reponse[y].className = "";
+                    historique[y+4].className = boule[y];
+                }
+    
+                dataReset();
+            }
+        })
+    }
+
+    // Bouton reset
+    reset.addEventListener('click', () => {
+        reponse.forEach(r => {
+            r.className = "";
+        })
+        dataReset();
+    });
+}
+
+jeu();
